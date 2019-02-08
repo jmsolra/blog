b02ed575c6ddb1eff76cc95195555fa999ca8713
jmsr <jsr@inesingenieros.com>
2019-02-08 14:21:31 +0100

    unir posts y urls

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const express = require('express')
-const mongoClient = require('./mongo')
+const redis = require('redis')
 const mongooseClient = require('./mongoose')
 const usersRouter = require('./users')
 const postsRouter = require('./posts')
+const urlsRouter = require('./urls')
+
 const app = express()
 
+// Mongo
 const dbConection = mongooseClient.connection
 dbConection.on('error', err => console.error(err))
 dbConection.on('open', () => console.log('Conectado a mongo'))
 
+// Redis
+const client = redis.createClient(32768, 'localhost')
+client.on('error', err => {
+  console.error(err)
+})
+client.on('connect', function() {
+  console.log('Redis client connected')
+})
+
 app.use(express.json())
-app.use('/posts', [postsRouter])
-
-//app.use('/posts', postsRouter)
-
+app.use('/posts', postsRouter)
 app.use('/users', usersRouter)
+app.use('/urls', urlsRouter(client))
 app.listen(3000, () =>
   console.log('Blog- Express listening on http://localhost:3000')
 )
