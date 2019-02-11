11466fca17114a37b7ab9dfcc86d7e2fde042e50
Robinson <rgb@inesingenieros.com>
2019-02-11 11:51:42 +0100

    prueba

--- index-redis.js
+++ index-redis.js
@@ -1,15 +1,15 @@
-const redis = require('redis')
-const express = require('express')
-const urls = require('./urls')
+const redis = require("redis")
+const express = require("express")
+const urls = require("./urls")
 
-const client = redis.createClient(32768, 'localhost')
+const client = redis.createClient(32768, "localhost")
 
-client.on('error', err => {
+client.on("error", err => {
   console.error(err)
 })
 
-client.on('connect', function() {
-  console.log('Redis client connected')
+client.on("connect", function() {
+  console.log("Redis client connected")
 })
 
 const app = express()
@@ -17,8 +17,6 @@ const app = express()
 app.use(express.json())
 
 // CREAR URL
-app.use('/urls', urls(client))
+app.use("/urls", urls(client))
 
-// GET /shortid
-
-app.listen(3000, () => console.log('Listo en el 3000'))
+app.listen(3000, () => console.log("Listo en el 3000"))
